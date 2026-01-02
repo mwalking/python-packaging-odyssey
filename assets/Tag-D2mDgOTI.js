703cc079af74299fe73b82d2a4c8677639a4830b
Mason <masonewalker@gmail.com>
2026-01-01 20:10:13 -0800

    Updates

---
+++ assets/Tag-D2mDgOTI.js
@@ -0,0 +1,6 @@
+import{c as e,j as s}from"./index-CKwBs8q3.js";/**
+ * @license lucide-react v0.309.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=e("ArrowRight",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]]),n=({children:t})=>s.jsx("span",{className:"inline-flex items-center px-2.5 py-1 rounded-full bg-indigo-50 text-indigo-700 text-xs font-semibold",children:t});export{i as A,n as T};
